c62b1a05bb2d34a6e17ce027eec780797b7399ac
Eyerusalem Debero <eyersalemmesele@gmail.com>
2026-04-17 22:52:46 -0500

    Remove all week folders

--- week-02/01_Ex1B_first_query.sql
+++
@@ -1,9 +0,0 @@
-/*
-sakila is a database used to collect information _____ and _____ for use by _____
-*/
-USE Sakila;
-SELECT * FROM actor;
-SELECT * FROM actor; -- Retrieved [number] records
-SELECT * From film;
-SELECT * FROM film; -- Retrived [number] records
-
